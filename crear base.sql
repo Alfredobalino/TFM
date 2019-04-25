e121ba55031675ecb94b8ab4872337c49b741a7e
javiee015 <47116097+javiee015@users.noreply.github.com>
2019-04-25 10:14:23 +0200

    Add files via upload

---
+++ crear base.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE mydb;
+USE mydb 
+CREATE TABLE localizacion (objectID INT PRIMARY KEY, rings MULTIPOLYGON);
+CREATE TABLE atributos (HOJA INT, ID INT, DLO VARCHAR (1000), OBJECTID INT PRIMARY KEY, column1 INT);
+INSERT INTO localizacion (resultado);
+INSERT INTO atributos (resultado1);
